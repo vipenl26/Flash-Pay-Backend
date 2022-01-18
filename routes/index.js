1557d22fb0dda186aaab4beede72ef5d2d7ec24e
Kapahimself <kapasy591@gmail.com>
2022-01-18 20:04:51 +0530

    Kapa's noob test

--- routes/index.js
+++ routes/index.js
@@ -2,6 +2,9 @@ var express = require('express');
 var router = express.Router();
 
 /* GET home page. */
+
+/* Poop sandwich */
+
 router.get('/', function(req, res, next) {
   res.render('index', { title: 'Express' });
 });
