06aa1bcde2e27069cd204b09a8597cc00cb28637
Kapahimself <kapasy591@gmail.com>
2022-01-19 22:54:19 +0530

    Added payment schema and payment routes

---
+++ models/payments.js
@@ -0,0 +1,37 @@
+const mongoose = require("mongoose");
+
+const paymentSchema = new mongoose.Schema({
+    paymentType: {
+        type: String,
+        required: true,
+        description: "This is the type of payment that can be done"
+    },
+
+    paymentSource: {
+        type: String,
+        required: true,
+        description: "Specifies the source of the transaction"
+    },
+
+    paymentDescription: {
+        type: String,
+        required: false,
+        description: "Specifies the description of the transaction"
+    },
+
+    amount: {
+        type: Number,
+        required: true,
+        description: "Specifies the amount of transaction"
+    },
+
+    dateOfTransaction: {
+        required: true,
+        description: "Records the date of transaction"
+    },
+});
+
+const Payments = mongoose.model("Payments", paymentSchema);
+
+
+module.exports = Payments;
---
+++ routes/payments.js
@@ -0,0 +1,12 @@
+"use strict";
+
+// importing required modules
+const express = require("express");
+const router = express.Router();
+const Payments = require("../models/payments.js");
+
+//default page that opens when user uses this route
+router.get("/", (req, res) => {
+    
+});
+
